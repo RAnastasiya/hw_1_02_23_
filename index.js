b43ba887f7c2c28bbd534056e757517a075058b1
RAnastasiya <rizhko.anastasiya@gmail.com>
2023-02-02 16:41:43 +0200

    add

--- index.js
+++ index.js
@@ -1,46 +1,89 @@
 "use strict";
 
-// const options = {
-//   breckets: {
-//     "(": ")",
-//     "[": "]",
-//     "{": "}",
+// const map1 = new Map();
+// console.log( map1);
+// console.log(typeof map1);
+// const dictionary = new Map();
+// dictionary.set('dog', 'пес');
+// dictionary.set('cat', 'кіт');
+// dictionary.set('forest', 'ліс');
+// dictionary.set('rat', 'щур');
+// dictionary.set('field', 'поле');
+// dictionary.set('funny', 'веселий');
+// dictionary.set('sad', 'сумний');
+// dictionary.set('happy', 'щасливий');
+// dictionary.set('in', 'у');
+// dictionary.set('at', 'в');
+// dictionary.set('morning', 'ранок');
+// dictionary.set('go', 'ідти');
+// dictionary.set('jump', 'стрибати');
+// dictionary.set('walk', 'гуляти');
+// dictionary.set('and', 'і');
+// console.log(dictionary);
+// const sentence  = "In  morning"
+// const translate = str => {
+//     let result = ''
+//     str.trim().toLowerCase().split(' ').forEach(el => {
+//         if(dictionary.has(el))
+//         if(el === '') result += ' '
+//         result += dictionary.get(el)
+//     });
+//     return result
+// }
+// console.log(translate(sentence));
+
+// const number1 = [ 1,3,6,3,5,7,23]
+// const number2 = [ 1,3,623,2435,5,7,23]
+// console.log([... new Set([...number1, ...number2])])
+
+const user = {
+  personalInfo: {
+    id: 12,
+    firstName: "Tom",
+    lastName: "Qwerrty",
+    bday: {
+      day: 28,
+      month: 6,
+      year: 1987,
+    },
+    gender: "male",
+  },
+  contactInfo: {
+    phone: "123-56-89",
+    email: "qwe@gmail.com",
+    address: {
+      town: "NY",
+      street: "Avenu 25",
+      house: 2546,
+    },
+  },
+  profession: "programmer",
+};
+
+// Destructuring object
+// const {
+//   personalInfo: { gender: userGender },
+//   contactInfo: {
+//     email,
+//     phone: userTel,
+//     address: { house: houseNumber },
 //   },
-// };
+// } = user;
+// console.log(userGender);
+// console.log(email);
+// console.log(userTel);
+// console.log(houseNumber);
 
-// /**
-//  *
-//  * @param {string} str
-//  * @param {object} options
-//  * @param {object} options.breckets
-//  * @returns {object}
-//  */
-// const checkExpression = (str, options) => {
-//   const stack = new Stack(str.length);
-//   const closesBreckets = Object.values(options.breckets);
-//   for (const symbol of str) {
-//     // console.log(symbol);
-//     if (options.breckets[symbol]) {
-//       stack.push(symbol);
-//       continue;
-//     }
-//     const lastSymbolInStack = stack.pick();
-//     const correctSymbol = options.breckets[lastSymbolInStack];
-//     if (symbol === correctSymbol) {
-//       stack.pop();
-//     } else if (closesBreckets.includes(symbol)) {
-//       return false;
-//     }
-//   }
-//   console.log(stack);
-//   return stack.isEmpty;
-// };
+// ... rest
+// const {
+//   contactInfo: { email },
+//   ...rest
+// } = user;
 
-// console.log(checkExpression(" &hh 4 ( gj[ 6 ^ ]    {})   88 ", options));
+// console.log(rest);
 
-
-const queue = new Queue(1,5,4);
-console.log(queue)
-console.log(queue.enQueue(77))
-console.log(queue.deQueue())
-console.log(queue)
+// Destructuring array
+// const colors = ["red", "teal", "blue", "aqua"];
+// const [red, , , aqua] = colors;
+// const [one, ...restColors] = colors;
+// console.log(restColors);
