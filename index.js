9e0dd78287ddfca9a44eb7e327897ec274270a6f
RAnastasiya <rizhko.anastasiya@gmail.com>
2023-02-02 18:11:47 +0200

    Add HW Destructuring

--- index.js
+++ index.js
@@ -1,66 +1,39 @@
 "use strict";
 
 /*
-Напишіть функцію, яка приймає два рядки
-і повертає true, якщо з літер першого рядка можна скласти другий і навпаки
-false - якщо не можна
-
-//compare('test', 'text') -> false
-//compare('Mom', 'omm') -> true
-//compare('asd', 'Sad') ->true
-//compare('asd', 'ssaadd') ->false
-//compare('ssaadd', 'asd') ->false
+ Створити змінні для року народження, імені другої дитини, робочого телефону.
+  Бажано одинм рядком, використовуючи деструктуризацію.
 */
+const user = {
+    id:1,
+    privateInfo:{
+      fname:"Brad",
+      sname:"Pitt",
+      bday:{
+        day:18,
+        month:12,
+        year:1963,
+      },
+      children:['Helen','Alex','Georg','Anna'],
+    },
+    contactInfo:{
+      phone:{
+        work:'123-12-45',
+        mobile:'005-002-003',
+      },
+      adress:{
+        town:'ZP',
+        street:'12Avenu',
+        house:45,
+      },
+      mail:'brad@gmail.com'
+    },
+    profession:'actor'
+  }
 
+  const {privateInfo:{bday:{year: yearOfBirth}, children: [,secondChild]}, contactInfo:{phone: {work: phoneWork}}} = user
 
-/**
- * 
- * @param {String} arr1 
- * @param {String} arr2 
- * @returns {Boolean}
- */
-const checkCompare = (arr1, arr2) => {
-    const dictionary1 = new Map()
-    arr1.forEach(el => dictionary1.set(el.toLowerCase(), el.toLowerCase()))
-    return !arr2.map(el => {
-        if (!dictionary1.has(el.toLowerCase())) return false
-    }).includes(false);
-}
-/**
- * 
- * @param {String} arr1 
- * @param {String} arr2 
- * @returns {Boolean}
- */
-const compare = ([...str1], [...str2]) => {
-    if(str1.length !== str2.length) return false
-    return checkCompare(str1, str2) && checkCompare(str2, str1)
-}
-
-console.log(compare('test', 'text')); // false
-console.log(compare("Mom", "omm")); // true
-console.log(compare("asd", "Sad")); // true
-console.log(compare('asd', 'ssaadd'));  // false
-
-
-/**
- * 
- * @param {String} param0 
- * @param {String} param1 
- * @returns {Boolean}
- */
-const compare2 = ([...str1], [...str2]) => {
-  if (str1.length !== str2.length) return false;
-  const dictionary1 = new Map();
-  str1.forEach((el) => dictionary1.set(el, el.toLowerCase()));
-  return !str2
-    .map((el) => {
-      if (!dictionary1.has(el.toLowerCase())) return false;
-    })
-    .includes(false);
-};
-
-console.log(compare2("test", "text")); // false
-console.log(compare2("Mom", "omm")); // true
-console.log(compare2("asd", "Sad")); // true
-console.log(compare2("asd", "ssaadd")); // false
+  console.log(yearOfBirth);
+  console.log(secondChild);
+  console.log(phoneWork);
+ 
