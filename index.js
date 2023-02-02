6f94dd0a60c0e889440274d38f68535c8e3e0ee8
RAnastasiya <rizhko.anastasiya@gmail.com>
2023-02-02 18:02:38 +0200

    Add HW

--- index.js
+++ index.js
@@ -1,89 +1,66 @@
 "use strict";
 
-// const map1 = new Map();
-// console.log( map1);
-// console.log(typeof map1);
-// const dictionary = new Map();
-// dictionary.set('dog', 'пес');
-// dictionary.set('cat', 'кіт');
-// dictionary.set('forest', 'ліс');
-// dictionary.set('rat', 'щур');
-// dictionary.set('field', 'поле');
-// dictionary.set('funny', 'веселий');
-// dictionary.set('sad', 'сумний');
-// dictionary.set('happy', 'щасливий');
-// dictionary.set('in', 'у');
-// dictionary.set('at', 'в');
-// dictionary.set('morning', 'ранок');
-// dictionary.set('go', 'ідти');
-// dictionary.set('jump', 'стрибати');
-// dictionary.set('walk', 'гуляти');
-// dictionary.set('and', 'і');
-// console.log(dictionary);
-// const sentence  = "In  morning"
-// const translate = str => {
-//     let result = ''
-//     str.trim().toLowerCase().split(' ').forEach(el => {
-//         if(dictionary.has(el))
-//         if(el === '') result += ' '
-//         result += dictionary.get(el)
-//     });
-//     return result
-// }
-// console.log(translate(sentence));
+/*
+Напишіть функцію, яка приймає два рядки
+і повертає true, якщо з літер першого рядка можна скласти другий і навпаки
+false - якщо не можна
 
-// const number1 = [ 1,3,6,3,5,7,23]
-// const number2 = [ 1,3,623,2435,5,7,23]
-// console.log([... new Set([...number1, ...number2])])
+//compare('test', 'text') -> false
+//compare('Mom', 'omm') -> true
+//compare('asd', 'Sad') ->true
+//compare('asd', 'ssaadd') ->false
+//compare('ssaadd', 'asd') ->false
+*/
 
-const user = {
-  personalInfo: {
-    id: 12,
-    firstName: "Tom",
-    lastName: "Qwerrty",
-    bday: {
-      day: 28,
-      month: 6,
-      year: 1987,
-    },
-    gender: "male",
-  },
-  contactInfo: {
-    phone: "123-56-89",
-    email: "qwe@gmail.com",
-    address: {
-      town: "NY",
-      street: "Avenu 25",
-      house: 2546,
-    },
-  },
-  profession: "programmer",
+
+/**
+ * 
+ * @param {String} arr1 
+ * @param {String} arr2 
+ * @returns {Boolean}
+ */
+const checkCompare = (arr1, arr2) => {
+    const dictionary1 = new Map()
+    arr1.forEach(el => dictionary1.set(el.toLowerCase(), el.toLowerCase()))
+    return !arr2.map(el => {
+        if (!dictionary1.has(el.toLowerCase())) return false
+    }).includes(false);
+}
+/**
+ * 
+ * @param {String} arr1 
+ * @param {String} arr2 
+ * @returns {Boolean}
+ */
+const compare = ([...str1], [...str2]) => {
+    if(str1.length !== str2.length) return false
+    return checkCompare(str1, str2) && checkCompare(str2, str1)
+}
+
+console.log(compare('test', 'text')); // false
+console.log(compare("Mom", "omm")); // true
+console.log(compare("asd", "Sad")); // true
+console.log(compare('asd', 'ssaadd'));  // false
+
+
+/**
+ * 
+ * @param {String} param0 
+ * @param {String} param1 
+ * @returns {Boolean}
+ */
+const compare2 = ([...str1], [...str2]) => {
+  if (str1.length !== str2.length) return false;
+  const dictionary1 = new Map();
+  str1.forEach((el) => dictionary1.set(el, el.toLowerCase()));
+  return !str2
+    .map((el) => {
+      if (!dictionary1.has(el.toLowerCase())) return false;
+    })
+    .includes(false);
 };
 
-// Destructuring object
-// const {
-//   personalInfo: { gender: userGender },
-//   contactInfo: {
-//     email,
-//     phone: userTel,
-//     address: { house: houseNumber },
-//   },
-// } = user;
-// console.log(userGender);
-// console.log(email);
-// console.log(userTel);
-// console.log(houseNumber);
-
-// ... rest
-// const {
-//   contactInfo: { email },
-//   ...rest
-// } = user;
-
-// console.log(rest);
-
-// Destructuring array
-// const colors = ["red", "teal", "blue", "aqua"];
-// const [red, , , aqua] = colors;
-// const [one, ...restColors] = colors;
-// console.log(restColors);
+console.log(compare2("test", "text")); // false
+console.log(compare2("Mom", "omm")); // true
+console.log(compare2("asd", "Sad")); // true
+console.log(compare2("asd", "ssaadd")); // false
